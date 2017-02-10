d9919de1c18d3dc5448da915ece4776cd729f799
wikiviggy <vgnshprabhakar@gmail.com>
2017-02-10 10:06:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool= require('pg');
+var Pool= require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
 var session = require('express-session');
