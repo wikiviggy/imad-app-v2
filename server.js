987d8125b5a1ce022253038ab2e4ad79ff562aa2
wikiviggy <vgnshprabhakar@gmail.com>
2017-02-13 10:37:42 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var config= {
     database:'wikiviggy',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:'db-wikiviggy-11704',
 };
 
 var app = express();
